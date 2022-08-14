d2c4392aee92599b8d7bb769311cf82439b1f571
maximsamus <maximsamus1@gmail.com>
2022-08-14 16:54:25 +0300

    UI fix

--- Dice SwiftUI/ContentView.swift
+++ Dice SwiftUI/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.95, green: 0.65, blue: 0.51)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("photo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300.0, height: 300.0)
+                    .clipShape(Circle())
+                    .overlay(Circle()
+                        .stroke(Color.black, lineWidth: 5))
+                    .padding()
+                Text("Maxim Samus")
+                    .font(Font.custom("BungeeInline-Regular", size: 50))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                Text("Ios Developer")
+                    .font(Font.custom("BungeeInline-Regular", size: 35))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .padding()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone SE"))
     }
 }
+
