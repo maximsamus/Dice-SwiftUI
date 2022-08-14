5b9aa2cae0cc7052e80dc770018584ad822784fa
maximsamus <maximsamus1@gmail.com>
2022-08-14 13:20:04 +0300

    Initial Commit

---
+++ Dice SwiftUI/Dice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_SwiftUIApp.swift
+//  Dice SwiftUI
+//
+//  Created by Максим Самусь on 14.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
